782b99fe85a49afcec371b50455fa48b0d4e8f22
Amarildo Lacerda <amarildo_51@msn.com>
2016-02-03 21:16:11 -0200

    Update inset100000A_amarildolacerda.sql

--- inset100000A_amarildolacerda.sql
+++ inset100000A_amarildolacerda.sql
@@ -1,16 +1,32 @@
+/*
+  Amarildo Lacerda
+  Objetivo:  adicionar 100.000 registro a base ja existente
+
+
+  Observar:    
+    -  como é possivel executar um trecho de codigo, que pode ir dentro uma   Query   (QUERY.SQL.TEXT), sem necessariamente
+       criar uma procedure;
+    - como o firebird monta um "CURSOR" automático e substitui as variáveis para serem utilizadas no LOOP;
+    - observar o controle de transação feita dentro do loop
+
+*/
+
+
 
 execute block
 as
-declare variable codigo varchar(18);
+/* variaveis */
+declare variable codigo varchar(18);   
 declare variable nome varchar(50);
 declare variable unidade varchar(5);
 declare variable preco double precision;
 begin
+  /* um cursor sem precisar declarar, para uso interno */
   for select first 100000 codigo,nome,unidade,preco from fb_prod
   into :codigo,:nome, :unidade, :preco
   do
   begin
-    in autonomous transaction do
+    in autonomous transaction do   /* controle de transacao */
     begin
      insert into fb_prod
        (codigo,nome,unidade,preco)
